44424b3a56b7abb06ba6d43bfab05a83db44d9d0
Tom Tsiliopoulos <tsiliopoulos@hotmail.com>
2017-02-11 12:22:22 -0500

    added game edit function

--- server/routes/games.js
+++ server/routes/games.js
@@ -20,13 +20,66 @@ router.get('/', (req, res, next) => {
       return console.error(err);
     }
     else {
-      res.render('content/gamelist', {
+      res.render('games/index', {
         title: 'Games',
         games: games
       });
 
     }
 
+  });
+
+});
+
+/* GET edit - show current game to edit */
+router.get('/:id', (req, res, next) => {
+
+  // get a reference to the id of the game to edit
+  let id = req.params.id;
+
+  // find the game to edit by it's id in the games collection
+  game.findById(id, (err, games) => {
+
+    if (err) {
+      console.error(err);
+      res.end(error);
+    }
+    else {
+      // show the edit view
+      res.render('games/details', {
+        title: 'Game Details',
+        games: games
+      });
+
+    }
+
+  });
+
+});
+
+/* POST edit - process the game to edit */
+router.post('/:id', (req, res, next) => {
+
+  // get a reference to the id of the game to edit
+  let id = req.params.id;
+
+  // create a new games object to hold the changes
+  let games = new game({
+    "_id": id,
+    "name": req.body.name,
+    "cost": req.body.cost,
+    "rating": req.body.rating
+  });
+
+  game.update({ _id: id}, games, (err) => {
+    if(err) {
+      console.log(err);
+      res.end(error);
+    }
+    else {
+      // refresh the games list
+      res.redirect('/games');
+    }
   });
 
 });
