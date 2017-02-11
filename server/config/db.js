855561c2817d030faccb76ca53126166da0d106b
Tom Tsiliopoulos <tsiliopoulos@hotmail.com>
2017-02-11 09:51:01 -0500

    changed db config to work with MLab

--- server/config/db.js
+++ server/config/db.js
@@ -1,5 +1,6 @@
 module.exports = {
-  // Local MongoDB
-  "URI" : "mongodb://localhost/videogames"
-  //MLab -> "URI": "mongodb://thomas:123456@ds054999.mlab.com:54999/videogames"
+  // Local MongoDB -> "URI" : "mongodb://localhost/videogames"
+
+  //MLab ->
+  "URI": "mongodb://thomas:123456@ds054999.mlab.com:54999/videogames"
 }
